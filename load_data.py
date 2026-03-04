a569456f3616ad0e501c984c88977e56032c21aa
Soraya Codo <soraya.codo@epitech.eu>
2026-03-04 14:58:51 +0100

    modele entraine sur des modeles brutes

--- load_data.py
+++ load_data.py
@@ -18,7 +18,7 @@ def data_loading():
     print("\n Aperçu des 5 premières lignes")
     print(df.head(25))
 
-    print("\n Informations structurelles (Types et Valeurs Manquantes)"
+    print("\n Informations structurelles (Types et Valeurs Manquantes)")
     print(df.info())
     return df
 
