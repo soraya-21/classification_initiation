2fa24dff986d4bbe3bf9bd879a5fec08e098b8b3
Your Name <you@example.com>
2026-03-05 14:00:24 +0100

    Add .gitignore and initial data analysis notebook; update data loading script

--- load_data.py
+++ load_data.py
@@ -2,6 +2,8 @@ import kagglehub
 import pandas as pd
 import os
 
+from IPython.core.display_functions import display
+
 path = kagglehub.dataset_download("sulianova/cardiovascular-disease-dataset")
 print("Path to dataset files:", path)
 
@@ -9,10 +11,11 @@ files = os.listdir(path)
 csv_file = [f for f in files if f.endswith('.csv')][0]
 full_path = os.path.join(path, csv_file)
 
-df = pd.read_csv(full_path)
+df = pd.read_csv(full_path,sep=';',decimal=',',quotechar='"', quoting=3, encoding='utf-8')
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+display(df.head())
 
-print("\n--- Aperçu des 5 premières lignes ---")
-print(df.head())
 
 print("\n--- Informations structurelles (Types et Valeurs Manquantes) ---")
 print(df.info())
